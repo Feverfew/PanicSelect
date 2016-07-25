1e26f54a36fca2601a7e7934a6c3877a989df488
Feverfew <alexlns99@yahoo.co.uk>
2016-07-26 01:30:41 +0300

    config change

--- PanicSelect/manage.py
+++ PanicSelect/manage.py
@@ -11,6 +11,3 @@ def make_shell_context():
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("runserver", Server())
-
-if __name__ == "__main__":
-    manager.run()
