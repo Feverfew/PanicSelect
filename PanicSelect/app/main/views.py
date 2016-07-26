8d31d326b709448c0e532b0d11a980b2b918f3ca
Feverfew <alexlns99@yahoo.co.uk>
2016-07-26 20:02:53 +0300

    riot.txt removed

--- PanicSelect/app/main/views.py
+++ PanicSelect/app/main/views.py
@@ -1,13 +1,9 @@
 from . import main
-from flask import make_response, send_file
+from flask import make_response
 
 @main.route('/')
 @main.route('/about')
 @main.route('/donate')
 def index():
     return make_response(open('app/templates/index.html').read())
-
-@main.route('/riot.txt')
-def riot():
-    return send_file('static/riot.txt')
 
