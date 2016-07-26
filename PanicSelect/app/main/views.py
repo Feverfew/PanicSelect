804775e26d480917591d38d6da5d60ed42365cb1
Feverfew <alexlns99@yahoo.co.uk>
2016-07-26 19:53:03 +0300

    riot verification and Diana fixed

--- PanicSelect/app/main/views.py
+++ PanicSelect/app/main/views.py
@@ -1,8 +1,13 @@
 from . import main
-from flask import make_response
+from flask import make_response, send_file
 
 @main.route('/')
 @main.route('/about')
 @main.route('/donate')
 def index():
     return make_response(open('app/templates/index.html').read())
+
+@main.route('/riot.txt')
+def riot():
+    return send_file('static/riot.txt')
+
