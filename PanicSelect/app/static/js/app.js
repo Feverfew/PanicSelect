46b7bd3d3cd034d09fc16f564ccc5afeffb0368c
Feverfew <alexlns99@yahoo.co.uk>
2016-08-04 16:03:59 +0300

    More text added, Janna/jarvan bug fixed

--- PanicSelect/app/static/js/app.js
+++ PanicSelect/app/static/js/app.js
@@ -72,6 +72,7 @@ angular.module('PanicSelect')
     '$mdToast', function ($scope, Champion, ChampionDetail, $mdDialog, $mdMedia,
      $q, $timeout, $mdToast) {
         var self = this;
+        $scope.readMore = false;
         self.simulateQuery = false;
         // list of `champion` value/display objects
         self.champion_list = loadAll();
@@ -97,7 +98,7 @@ angular.module('PanicSelect')
          * Build `states` list of key/value pairs
          */
         function loadAll() {
-            var champion_list = "Aatrox, Ahri, Akali, Alistar, Amumu, Anivia, Annie, Ashe, Aurelion Sol, Azir, Bard, Blitzcrank, Brand, Braum, Caitlyn, Cassiopeia, Cho'Gath, Corki, Darius, Diana, Dr. Mundo, Draven, Ekko, Elise, Evelynn, Ezreal, Fiddlesticks, Fiora, Fizz, Galio, Gangplank, Garen, Gnar, Gragas, Graves, Hecarim, Heimerdinger, Illaoi, Irelia, Janna Jarvan IV, Jax, Jayce, Jhin, Jinx, Kalista, Karma, Karthus, Kassadin, Katarina, Kayle, Kennen, Kha'Zix, Kindred, Kog'Maw, LeBlanc, Lee Sin, Leona, Lissandra, Lucian, Lulu, Lux, Malphite, Malzahar, Maokai, Master Yi, Miss Fortune, Mordekaiser, Morgana, Nami, Nasus, Nautilus, Nidalee, Nocturne, Nunu, Olaf, Orianna, Pantheon, Poppy, Quinn, Rammus, Rek'Sai, Renekton, Rengar, Riven, Rumble, Ryze, Sejuani, Shaco, Shen, Shyvana, Singed, Sion, Sivir, Skarner, Sona, Soraka, Swain, Syndra, Tahm Kench, Taliyah, Talon, Taric, Teemo, Thresh, Tristana, Trundle, Tryndamere, Twisted Fate, Twitch, Udyr, Urgot, Varus, Vayne, Veigar, Vel'Koz, Vi, Viktor, Vladimir, Volibear, Warwick, Wukong, Xerath, Xin Zhao, Yasuo, Yorick, Zac, Zed, Ziggs, Zilean, Zyra";
+            var champion_list = "Aatrox, Ahri, Akali, Alistar, Amumu, Anivia, Annie, Ashe, Aurelion Sol, Azir, Bard, Blitzcrank, Brand, Braum, Caitlyn, Cassiopeia, Cho'Gath, Corki, Darius, Diana, Dr. Mundo, Draven, Ekko, Elise, Evelynn, Ezreal, Fiddlesticks, Fiora, Fizz, Galio, Gangplank, Garen, Gnar, Gragas, Graves, Hecarim, Heimerdinger, Illaoi, Irelia, Janna, Jarvan IV, Jax, Jayce, Jhin, Jinx, Kalista, Karma, Karthus, Kassadin, Katarina, Kayle, Kennen, Kha'Zix, Kindred, Kog'Maw, LeBlanc, Lee Sin, Leona, Lissandra, Lucian, Lulu, Lux, Malphite, Malzahar, Maokai, Master Yi, Miss Fortune, Mordekaiser, Morgana, Nami, Nasus, Nautilus, Nidalee, Nocturne, Nunu, Olaf, Orianna, Pantheon, Poppy, Quinn, Rammus, Rek'Sai, Renekton, Rengar, Riven, Rumble, Ryze, Sejuani, Shaco, Shen, Shyvana, Singed, Sion, Sivir, Skarner, Sona, Soraka, Swain, Syndra, Tahm Kench, Taliyah, Talon, Taric, Teemo, Thresh, Tristana, Trundle, Tryndamere, Twisted Fate, Twitch, Udyr, Urgot, Varus, Vayne, Veigar, Vel'Koz, Vi, Viktor, Vladimir, Volibear, Warwick, Wukong, Xerath, Xin Zhao, Yasuo, Yorick, Zac, Zed, Ziggs, Zilean, Zyra";
             return champion_list.split(/, +/g).map(function (champion_list) {
                 return {
                     value: champion_list.toLowerCase(),
@@ -113,6 +114,13 @@ angular.module('PanicSelect')
             return function filterFn(champion_list) {
                 return (champion_list.value.indexOf(lowercaseQuery) === 0);
             };
+        }
+        $scope.showMoreText = function () {
+            if ($scope.readMore === false) {
+                $scope.readMore = true;
+            } else {
+                $scope.readMore = false;
+            }
         }
         $scope.champions = null;
         $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
